ae045d613d31255fe8af433a02f67f04af2555ed
Regor <regorm88@gmail.com>
2015-07-15 16:39:08 +0200

    first commit

---
+++ Assets/Scripts/ControllCube.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllCube : MonoBehaviour {
+
+	public float xMax;
+	public float xMin;
+	
+	public void move(){
+		if(transform.position.x == xMax){
+			Debug.Log ("MoveLeft");
+			GetComponent<Rigidbody>().MovePosition(new Vector3(xMin,0.0f,0.0f));
+		}else{
+			GetComponent<Rigidbody>().MovePosition(new Vector3(xMax,0.0f,0.0f));
+		}
+	}
+}
---
+++ Assets/Scripts/TouchArea.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+// 
+public class TouchArea : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler{
+
+	public float smoothing;
+	public ControllCube controllCube;
+
+	private Vector2 origin;
+	private Vector2 direction;
+	private Vector2 smoothDirection;
+	private bool touched;
+	private int pointerID;
+
+	void Awake()
+	{
+		direction = Vector2.zero;
+		touched = false;
+	}
+	
+	public void OnPointerDown(PointerEventData data)
+	{
+		if (!touched)
+		{
+			touched = true;
+			pointerID = data.pointerId;
+			origin = data.position;
+		}
+	}
+	
+	public void OnDrag(PointerEventData data)
+	{
+		if (data.pointerId == pointerID)
+		{
+			Vector2 currentPosition = data.position;
+			Vector2 directionRaw = currentPosition - origin;
+			direction = directionRaw.normalized;
+			if(direction != Vector2.up ){
+				Debug.Log ("Shoot");
+			}
+		}
+	}
+	
+	public void OnPointerUp(PointerEventData data)
+	{
+		if (data.pointerId == pointerID)
+		{
+			direction = Vector3.zero;
+			touched = false;
+			controllCube.move();
+		}
+	}
+	
+	public Vector2 GetDirection()
+	{
+		smoothDirection = Vector2.MoveTowards(smoothDirection, direction, smoothing);
+		return smoothDirection;
+	}
+
+}
